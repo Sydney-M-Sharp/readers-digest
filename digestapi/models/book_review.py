c5d98f8ca7f697ea57c18a245ba3f5eb4bd8ed4f
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-06-20 16:41:29 -0500

    4th step in class work

--- digestapi/models/book_review.py
+++ digestapi/models/book_review.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 class BookReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="books_reviewed")
     book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name='book_reviews')
     rating = models.IntegerField()
     comment = models.CharField(max_length=255)
-    date_posted = models.DateField()
+    date_posted = models.DateTimeField(default=timezone.now)
     
